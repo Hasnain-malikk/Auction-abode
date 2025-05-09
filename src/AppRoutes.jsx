f027984969f9407f57c4ac11a5e49ac7d8757571
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2025-05-09 12:26:58 +0500

    updated

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -5,8 +5,8 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import PageNotFound from "./pages/PageNotFound";
-import AboutUs from "./pages/AboutUs";
-import ContactUs from "./pages/ContactUs";
+// import AboutUs from "./pages/AboutUs";
+// import ContactUs from "./pages/ContactUs";
 import Properties from "./pages/Properties";
 import Profile from "./pages/Profile";
 import ChangePassword from "./pages/ChangePassword";
@@ -24,8 +24,8 @@ const AnimatedRoutes = () => {
         <Route path="/changepassword" element={<ChangePassword />} />
         
         <Route path="/register" element={<Register />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/aboutus" element={<PageNotFound />} />
+        <Route path="/contactus" element={<PageNotFound />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </AnimatePresence>
